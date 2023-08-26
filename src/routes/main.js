650bbc74b3b93e11e11ce5cdac6cc4677d75acfe
mansiiawasthi <mansiawasthi55@gmail.com>
2023-08-26 13:09:04 +0530

    seven commit

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const { route } = require('express/lib/application')
-const Detail = require("../models/Details")
-const Slider = require("../models/slider")
-const Service = require("../models/Service")
-const Contact = require("../models/Contact")
-const routes = express.Router()
+// const express = require('express')
+// const { route } = require('express/lib/application')
+// const Detail = require("../models/Details")
+// const Slider = require("../models/slider")
+// const Service = require("../models/Service")
+// const Contact = require("../models/Contact")
+// const routes = express.Router()
+const express = require('express');
+const path = require('path');  // Make sure to require the 'path' module
+
+const { route } = require('express/lib/application');
+const Detail = require("../models/Details");
+const Slider = require("../models/slider");
+const Service = require("../models/Service");
+const Contact = require("../models/Contact");
+const routes = express.Router();
+
+// Debugging code to check resolved paths
+console.log("__dirname:", __dirname);
+console.log("Resolved path:", path.resolve('../models/slider'));
 routes.get("/", async(req, res) => {
     const details = await Detail.findOne({
         "_id": "64d91a93c213a234c99aae81"
